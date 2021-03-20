d814d1e5aec28df4fd932f889dcb139bf8e90f14
nikhil <nikhiltomy@au-ki.com>
2021-03-20 19:54:40 +0530

    first commit

---
+++ archive.php
@@ -0,0 +1,27 @@
+<?php
+get_header();
+?>
+
+
+<article class="content px-3 py-5 p-md-5">
+    
+<?php 
+if ( have_posts() ) : 
+    while ( have_posts() ) : the_post(); 
+        // Display post content
+		get_template_part('template-parts/content','archive');
+    endwhile; 
+endif; 
+?>
+
+</article>
+
+<?php
+get_footer();
+?>
+
+
+
+</body>
+
+</html>
---
+++ front-page.php
@@ -0,0 +1,27 @@
+<?php
+get_header();
+?>
+
+
+<article class="content px-3 py-5 p-md-5">
+    
+<?php 
+if ( have_posts() ) : 
+    while ( have_posts() ) : the_post(); 
+        // Display post content
+		the_content();
+    endwhile; 
+endif; 
+?>
+
+</article>
+
+<?php
+get_footer();
+?>
+
+
+
+</body>
+
+</html>
